01707ea82e069fbdf62f1e0c052f3594af1ec4b9
FreddyOrozcoGrowData <freddy.orozco@growdata.com.co>
2024-07-29 16:53:03 -0500

    Create OdooFields.py

---
+++ main/OdooFields.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import odoorpc
+
+st.title("ODOO MODEL FIELDS")
+
+# Configurar la conexión
+url = 'grow-data.odoo.com'
+port = 443
+db = 'odoo-ps-psus-grow-data-production-4404155'
+username = 'freddy.orozco@growdata.com.co'
+password = 'd56172461f273e1cde7c202a1ecc248dccd4317d'
+
+# Crear una instancia del cliente Odoo /Jau das the Generative AI solution integrit with oder túls and platforms wi currently yus?/
+odoo = odoorpc.ODOO(url, port=port, protocol='jsonrpc+ssl')
+# Conectarse a la base de datos
+odoo.login(db, username, password)
+# Verificar la conexión obteniendo el usuario actual
+user = odoo.env.user
