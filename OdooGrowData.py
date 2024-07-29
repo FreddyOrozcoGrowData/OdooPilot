eed31b9bb6765d45b59043cbd90964d838135ece
FreddyOrozcoGrowData <freddy.orozco@growdata.com.co>
2024-07-29 11:56:12 -0500

    Update OdooGrowData.py

--- OdooGrowData.py
+++ OdooGrowData.py
@@ -1,7 +1,24 @@
 import streamlit as st
 import pandas as pd
-
+import odoorpc
 
 st.set_page_config(layout='wide')
 
-st.title("ODOO GROW DATA")
+# Configurar la conexión
+url = 'grow-data.odoo.com'
+port = 443
+db = 'odoo-ps-psus-grow-data-production-4404155'
+username = 'freddy.orozco@growdata.com.co'
+password = 'd56172461f273e1cde7c202a1ecc248dccd4317d'
+
+# Crear una instancia del cliente Odoo
+odoo = odoorpc.ODOO(url, port=port, protocol='jsonrpc+ssl')
+
+# Conectarse a la base de datos
+odoo.login(db, username, password)
+
+# Verificar la conexión obteniendo el usuario actual
+user = odoo.env.user
+#print(f'Conectado como: {user.name} (ID: {user.id})')
+
+st.write("Conectado como: {user.name} (ID: {user.id})")
