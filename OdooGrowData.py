570fd772624a28f96271d90775620d8f99002df6
FreddyOrozcoGrowData <freddy.orozco@growdata.com.co>
2024-07-29 16:40:12 -0500

    Update OdooGrowData.py

--- OdooGrowData.py
+++ OdooGrowData.py
@@ -9,13 +9,10 @@ Created on Mon Jul  29 16:30:41 2024
 import streamlit as st
 st.set_page_config(layout="wide")
 navigation_tree = {
-    "Main": [
+    "Odoo Grow Data": [
         st.Page("main/OdooCRM.py", title="Odoo CRM"),   
         st.Page("main/OdooClientes.py", title="Odoo Clientes")]
 }
 
 nav = st.navigation(navigation_tree, position="sidebar")
 nav.run()
-
-
-st.title("GROW DATA", position="sidebar")
