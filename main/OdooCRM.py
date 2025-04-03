2f22d552510efa6673ac0991897ec049a7e46194
FreddyOrozcoGrowData <freddy.orozco@growdata.com.co>
2025-04-03 11:42:56 -0500

    Update OdooCRM.py

--- main/OdooCRM.py
+++ main/OdooCRM.py
@@ -27,17 +27,12 @@ lead_ids = odoo.env['crm.lead'].search([])
 leads = odoo.env['crm.lead'].read(lead_ids, ['name', 'email_from', 'phone', 'partner_id', 'user_id', 'x_studio_linea', 'stage_id', 'team_id', 'x_studio_tipo_de_oportunidad', 'x_studio_edopreventa', 'x_studio_preventa', 'create_date', 'expected_revenue', 'x_studio_consultoria_cop', 'x_studio_datos_cop', 'x_studio_ti_cop', 'x_studio_alcance', 'x_studio_objeto', 'date_deadline', 'x_studio_fecha_efectiva_de_cierre', 'date_closed', 'write_date', 'x_studio_tipo_de_producto', 'x_studio_proyecto', 'won_status', 'write_date'])
 
 
-
-# Mostrar los valores y tipos de la columna 'Cliente' usando st.write
-for index, value in enumerate(df_leads['Cliente']):
-    st.write(f"Índice: {index}, Valor: {value}, Tipo: {type(value)}")
-
 lead_data = [{
  'ID': lead['id'],
  'Nombre': lead['name'],
  'Correo': lead['email_from'],
  'Teléfono': lead['phone'],
- 'Cliente': lead['partner_id'],
+ 'Cliente': lead['partner_id'][1] if lead['partner_id'] else '',
  'Comercial': lead['user_id'][1] if lead['user_id'] else '',
  'Línea': lead['x_studio_linea'],
  'Etapa': lead['stage_id'][1] if lead['stage_id'] else '',
