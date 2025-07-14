82775297367aca19f65a5577783385c1e739b024
FreddyOrozcoGrowData <freddy.orozco@growdata.com.co>
2025-07-14 11:32:00 -0500

    Update OdooCRM.py

--- main/OdooCRM.py
+++ main/OdooCRM.py
@@ -22,11 +22,12 @@ user = odoo.env.user
 #Consulta oportunidades
 lead_ids = odoo.env['crm.lead'].search([])
 st.write(lead_ids)
-leads = odoo.env['crm.lead'].read(lead_ids, ['name', 'email_from', 'phone', 'partner_id', 'user_id', 'x_studio_linea', 'stage_id', 'team_id', 'x_studio_tipo_de_oportunidad', 'x_studio_edopreventa', 'x_studio_preventa', 'create_date', 'expected_revenue', 'x_studio_consultoria_cop', 'x_studio_datos_cop', 'x_studio_ti_cop', 'x_studio_alcance', 'x_studio_objeto', 'date_deadline', 'x_studio_fecha_efectiva_de_cierre', 'date_closed', 'write_date', 'x_studio_tipo_de_producto', 'x_studio_proyecto', 'won_status', 'write_date'])
+leads = odoo.env['crm.lead'].read(lead_ids, ['active', 'name', 'email_from', 'phone', 'partner_id', 'user_id', 'x_studio_linea', 'stage_id', 'team_id', 'x_studio_tipo_de_oportunidad', 'x_studio_edopreventa', 'x_studio_preventa', 'create_date', 'expected_revenue', 'x_studio_consultoria_cop', 'x_studio_datos_cop', 'x_studio_ti_cop', 'x_studio_alcance', 'x_studio_objeto', 'date_deadline', 'x_studio_fecha_efectiva_de_cierre', 'date_closed', 'write_date', 'x_studio_tipo_de_producto', 'x_studio_proyecto', 'won_status', 'write_date'])
 
 
 lead_data = [{
  'ID': lead['id'],
+ 'Activo': lead['active'],
  'Nombre': lead['name'],
  'Correo': lead['email_from'],
  'Teléfono': lead['phone'],
