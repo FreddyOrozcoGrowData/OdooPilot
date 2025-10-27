16659f01692f7aaa133ab26440d6c2fdf26cfb69
FreddyOrozcoGrowData <freddy.orozco@growdata.com.co>
2025-10-27 10:53:55 -0500

    Update OdooPresales.py

--- main/OdooPresales.py
+++ main/OdooPresales.py
@@ -25,7 +25,7 @@ st.divider()
 lead_ids = odoo.env['crm.lead'].with_context(active_test=False).search([])
 
 #st.write(lead_ids)
-leads = odoo.env['crm.lead'].read(lead_ids, ['active', 'name', 'partner_id', 'user_id', 'x_studio_linea', 'stage_id', 'team_id', 'x_studio_edopreventa', 'x_studio_preventa', 'x_studio_fecha_asignacion_preventa', 'x_studio_tipo_de_oportunidad', 'create_date', 'x_studio_fecha_entregap', 'x_vinculante_novinculante', 'expected_revenue', 'x_studio_consultoria_cop', 'x_studio_datos_cop', 'x_studio_ti_cop', 'date_deadline', 'write_date', 'x_studio_tipo_de_producto', 'won_status', 'write_date'])
+leads = odoo.env['crm.lead'].read(lead_ids, ['active', 'name', 'partner_id', 'user_id', 'x_studio_linea', 'stage_id', 'team_id', 'x_studio_edopreventa', 'x_studio_preventa', 'x_studio_fecha_asignacion_preventa', 'x_studio_tipo_de_oportunidad', 'create_date', 'x_studio_fecha_entregap', 'x_vinculante_novinculante', 'date_deadline', 'write_date', 'x_studio_tipo_de_producto', 'won_status', 'write_date'])
 
 lead_data = [{
  'ID': lead['id'],
@@ -42,10 +42,6 @@ lead_data = [{
  'Vinculante/No Vinculante': lead['x_vinculante_novinculante'],
  'Línea': lead['x_studio_linea'],
  'Etapa': lead['stage_id'][1] if lead['stage_id'] else '',
- 'Ingresos Esperados': lead['expected_revenue'],
- 'Consultoría (COP$)': lead['x_studio_consultoria_cop'],
- 'Datos (COP$)': lead['x_studio_datos_cop'],
- 'TI (COP$)': lead['x_studio_ti_cop'],
  'Tipo Oportunidad': lead['x_studio_tipo_de_oportunidad'],
  'Equipo de Ventas': lead['team_id'][1] if lead['team_id'] else '',
  'Última Modificación el': lead['write_date'],
